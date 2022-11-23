df266ada59d05c38fe373bf503165283617bd41a
23133ZK <115622123+23133ZK@users.noreply.github.com>
2022-11-23 18:19:43 +0800

    Add files via upload

---
+++ fuck.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+	int arr[3][3];
+	int i = 0,j=0,sum=0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			scanf("%d", &arr[i][j]);
+	}
+	sum = arr[0][0] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][2];
+	printf("%d", sum);
+	return 0;
+}
